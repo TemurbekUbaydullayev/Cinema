d8bb783cf2e89d4d90b277f06a5a14bd6e5e9835
Temur <ubaydullayev117@gmail.com>
2022-06-15 07:09:30 +0500

    endi tugadi

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -14,54 +14,6 @@ namespace Cinema
             Console.ForegroundColor = ConsoleColor.White;
 
             new MainMenu().Menu();
-
-
-            #region nimadir
-            //IAdminService adminService = new AdminService();
-
-            //AdminForCreationDto adminDto = new AdminForCreationDto()
-            //{
-            //    Id = 2,
-            //    FirstName = "Otabek",
-            //    LastName = "Sandiyev",
-            //    Age = 22,
-            //    Phone = "+998973452132",
-            //    Email = "otabek@gmail.com",
-            //    Password = "otash"
-            //};
-
-            //var admin = adminService.Update(adminDto);
-
-            //Console.WriteLine($"{admin.Id} {admin.CreatedAt} {admin.FirstName}");
-
-            //IMovieService movieService = new MovieService();
-
-            //MovieDto movieDto = new MovieDto()
-            //{
-            //    Name = "Jumanji",
-            //    Description = "Galivut kinosi",
-            //    Price = 11900,
-            //    Genre = Genre.Comedy,
-            //    StartTime = "Mon, 13 - 00"
-            //};
-
-            //var movie = movieService.Create(movieDto);
-
-            //Console.WriteLine($"{movie.Id} {movie.CreatedAt} {movie.Name}");
-
-            //var res = "Um".SearchOfMovie();
-
-            ////foreach (var item in res)
-            //Console.WriteLine($"{res[0]}");
-
-            //IAdminRepository admin = new AdminRepository();
-
-            //var res = admin.Get(2);
-
-            //var result = "temur".Login("otabek@gmail.com");
-
-            //Console.WriteLine($"{result}");
-            #endregion
         }
     }
 }
